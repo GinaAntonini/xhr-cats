1f2ac300e13f0280661eca98ee44bcf02d230f57
Gina Antonini <ginamantonini@gmail.com>
2017-08-22 21:08:03 -0500

    added json files

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-var cats = [
-	{name: "Fluffy", color: "Green", url: "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"},
-	{name: "Minton", color: "Gray", url: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
-	{name: "CatCat", color: "Orange", url: "http://www.rd.com/wp-content/uploads/sites/2/2016/02/06-train-cat-shake-hands.jpg"}
-];
-
-var katKoralDiv = document.getElementById("kat-koral");
 
 function domString(catArray){
 	var catString = "";
-	for (var i = 0; i < cats.length; i++){
+	for (var i = 0; i < catArray.length; i++){
 		var catCard = catArray[i];
 		catString+=`<div class="kat" id="catCard-${i}">`;
 		catString+=`</div>`;
@@ -20,9 +13,29 @@ function domString(catArray){
 }
 
 function writeToDom(theCatString){
-	katKoralDiv.innerHTML = theCatString;
+	var katKoralDiv = document.getElementById("kat-koral");
+	katKoralDiv.innerHTML += theCatString;
 }
 
-domString(cats);
 
+function executeThisCodeAfterFileLoads(){
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+}
+//here, we looked into the console, typed this.responseText and got the json info, then we typed data. the array popped up under that. Therefore we can access this array by data.cats
 
+function exectureThisCodeIfFileErrors(){
+	console.log("Shit broke");
+}
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", exectureThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest2.addEventListener("error", exectureThisCodeIfFileErrors);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
+//this tells of the XML request and what to do once it's been retrieved. The last line means run the request
